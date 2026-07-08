c632acd23c2d8395d9b7977f36e33adcb110a5cf
legendlv001 <legendlv001@gmail.com>
2026-07-08 11:33:59 +0800

    Refactor service worker for caching and notifications

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,39 @@
-// ======================================
+// ==========================================
 // service-worker.js
-// Crazy Fishing Simulator v1.0
-// ======================================
+// Crazy Fishing Simulator
+// ==========================================
 
-const CACHE_NAME = "crazy-fishing-v1";
+"use strict";
 
-const FILES_TO_CACHE = [
+const CACHE_NAME = "crazy-fishing-v1.0.0";
+
+const FILES = [
 
     "./",
+
     "./index.html",
+
     "./style.css",
+
     "./app.js",
+
     "./game.js",
-    "./fish.js",
+
     "./player.js",
+
+    "./fish.js",
+
+    "./fishRenderer.js",
+
     "./storage.js",
+
     "./ui.js",
+
     "./manifest.json",
 
-    "./assets/icons/icon-72.png",
-    "./assets/icons/icon-96.png",
-    "./assets/icons/icon-128.png",
-    "./assets/icons/icon-144.png",
-    "./assets/icons/icon-152.png",
-    "./assets/icons/icon-192.png",
-    "./assets/icons/icon-384.png",
-    "./assets/icons/icon-512.png",
+    "./assets/logo.svg",
 
-    "./assets/images/screenshot1.png",
-    "./assets/images/screenshot2.png",
-
-    "./assets/sounds/cast.mp3",
-    "./assets/sounds/hook.mp3",
-    "./assets/sounds/catch.mp3",
-    "./assets/sounds/button.mp3",
-    "./assets/sounds/bgm.mp3"
+    "./assets/icon.svg"
 
 ];
 
@@ -42,212 +41,116 @@ const FILES_TO_CACHE = [
 // Install
 // ==========================
 
-self.addEventListener("install",(event)=>{
+self.addEventListener(
 
-    event.waitUntil(
+    "install",
 
-        caches.open(CACHE_NAME)
+    event => {
 
-        .then(cache=>{
+        event.waitUntil(
 
-            return cache.addAll(FILES_TO_CACHE);
+            caches.open(CACHE_NAME)
 
-        })
+                .then(cache => {
 
-    );
+                    return cache.addAll(FILES);
 
-    self.skipWaiting();
+                })
 
-});
+        );
+
+        self.skipWaiting();
+
+    }
+
+);
 
 // ==========================
 // Activate
 // ==========================
 
-self.addEventListener("activate",(event)=>{
+self.addEventListener(
 
-    event.waitUntil(
+    "activate",
 
-        caches.keys()
+    event => {
 
-        .then(keys=>{
+        event.waitUntil(
 
-            return Promise.all(
+            caches.keys().then(keys =>
 
-                keys.map(key=>{
+                Promise.all(
 
-                    if(key!==CACHE_NAME){
+                    keys.map(key => {
 
-                        return caches.delete(key);
+                        if (key !== CACHE_NAME) {
 
-                    }
+                            return caches.delete(key);
 
-                })
+                        }
 
-            );
+                    })
 
-        })
+                )
 
-    );
+            )
 
-    self.clients.claim();
+        );
 
-});
+        self.clients.claim();
+
+    }
+
+);
 
 // ==========================
 // Fetch
 // ==========================
 
-self.addEventListener("fetch",(event)=>{
-
-    if(event.request.method!=="GET") return;
-
-    event.respondWith(
-
-        caches.match(event.request)
-
-        .then(cacheResponse=>{
-
-            if(cacheResponse){
-
-                return cacheResponse;
-
-            }
-
-            return fetch(event.request)
-
-            .then(networkResponse=>{
-
-                const clone=networkResponse.clone();
-
-                caches.open(CACHE_NAME)
-
-                .then(cache=>{
-
-                    cache.put(event.request,clone);
-
-                });
-
-                return networkResponse;
-
-            })
-
-            .catch(()=>{
-
-                if(event.request.destination==="document"){
-
-                    return caches.match("./index.html");
-
-                }
-
-            });
-
-        })
-
-    );
-
-});
-
-// ==========================
-// Push Notification
-// ==========================
-
-self.addEventListener("push",(event)=>{
-
-    const data=event.data
-        ?event.data.text()
-        :"Crazy Fishing Simulator";
-
-    event.waitUntil(
-
-        self.registration.showNotification(
-
-            "Crazy Fishing",
-
-            {
-
-                body:data,
-
-                icon:"./assets/icons/icon-192.png",
-
-                badge:"./assets/icons/icon-192.png",
-
-                vibrate:[200,100,200],
-
-                requireInteraction:false
-
-            }
-
-        )
-
-    );
-
-});
-
-// ==========================
-// Notification Click
-// ==========================
-
 self.addEventListener(
 
-    "notificationclick",
+    "fetch",
 
-    (event)=>{
+    event => {
 
-        event.notification.close();
+        event.respondWith(
 
-        event.waitUntil(
+            caches.match(event.request)
 
-            clients.openWindow("./")
+                .then(response => {
+
+                    if (response) {
+
+                        return response;
+
+                    }
+
+                    return fetch(event.request)
+
+                        .then(networkResponse => {
+
+                            return caches.open(CACHE_NAME)
+
+                                .then(cache => {
+
+                                    cache.put(
+
+                                        event.request,
+
+                                        networkResponse.clone()
+
+                                    );
+
+                                    return networkResponse;
+
+                                });
+
+                        });
+
+                })
 
         );
 
     }
 
 );
-
-// ==========================
-// Background Sync
-// ==========================
-
-self.addEventListener(
-
-    "sync",
-
-    (event)=>{
-
-        if(event.tag==="save-game"){
-
-            event.waitUntil(
-
-                Promise.resolve()
-
-            );
-
-        }
-
-    }
-
-);
-
-// ==========================
-// Message
-// ==========================
-
-self.addEventListener(
-
-    "message",
-
-    (event)=>{
-
-        if(event.data==="skipWaiting"){
-
-            self.skipWaiting();
-
-        }
-
-    }
-
-);
-
-console.log("Service Worker aktif");
